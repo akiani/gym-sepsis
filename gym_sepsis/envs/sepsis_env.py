31f7a3c1383d32ec0f91b1b5106919edd1784f38
akiani <akiani@users.noreply.github.com>
2019-03-17 19:51:06 -0700

    add state index

--- gym_sepsis/envs/sepsis_env.py
+++ gym_sepsis/envs/sepsis_env.py
@@ -12,7 +12,7 @@ TERMINATION_MODEL = "model/sepsis_termination.model"
 OUTCOME_MODEL = "model/sepsis_outcome.model"
 STARTING_STATES_VALUES = "model/sepsis_starting_states.npz"
 
-NUM_FEATURES = 46
+NUM_FEATURES = 48  # 46 + action + state index
 NUM_ACTIONS = 24
 
 EPISODE_MEMORY = 10
@@ -38,18 +38,18 @@ class SepsisEnv(gym.Env):
         self.action_space = spaces.Discrete(24)
 
         # use a pixel to represent next state
-        self.observation_space = spaces.Box(low=0, high=24, shape=(46, 1, 1),
+        self.observation_space = spaces.Box(low=0, high=NUM_ACTIONS, shape=(NUM_FEATURES, 1, 1),
                                             dtype=np.float32)
         self.reset(starting_state=starting_state)
         return
 
     def step(self, action):
         # create memory of present
-        self.memory.append(np.append(self.s.reshape((1, 46)), action))
+        self.memory.append(np.append(np.append(self.s.reshape((1, NUM_FEATURES - 2)), action), self.state_idx))
         if self.verbose:
             print("running on memory: ", self.memory)
 
-        next_state = self.state_model.predict(np.expand_dims(self.memory, 0))
+        next_state = self.state_model.predict(np.expand_dims(self.memory, 0)[:, :, :-1])
         termination = self.termination_model.predict(np.expand_dims(self.memory, 0))
         outcome = self.outcome_model.predict(np.expand_dims(self.memory, 0))
 
@@ -80,13 +80,14 @@ class SepsisEnv(gym.Env):
         self.rewards = []
         self.dones = []
         self.state_idx = 0
-        self.memory = deque([np.zeros(shape=[NUM_FEATURES + 1])] * 10, maxlen=10)
+        self.memory = deque([np.zeros(shape=[NUM_FEATURES])] * 10, maxlen=10)
+
         if starting_state is None:
             self.s = self.starting_states[np.random.randint(0, len(self.starting_states))][:-1]
         else:
             self.s = starting_state
 
-        self.s = self.s.reshape(46, 1, 1)
+        self.s = self.s.reshape(NUM_FEATURES -2, 1, 1)
 
         if self.verbose:
             print("starting state:", self.s)
@@ -105,6 +106,6 @@ class SepsisEnv(gym.Env):
                    'race_white', 'race_black', 'race_hispanic', 'race_other', 'height',
                    'weight', 'vent', 'sofa', 'lods', 'sirs', 'qsofa', 'qsofa_sysbp_score',
                    'qsofa_gcs_score', 'qsofa_resprate_score', 'elixhauser_hospital',
-                   'blood_culture_positive', 'action']
+                   'blood_culture_positive', 'action', 'state_idx']
         df = pd.DataFrame(self.memory, columns=columns, index=range(0, 10))
         print(df)
